fb618d9f6ba8d85372eb72b4689b69961f671e9c
Asiya <asiyabanus007@gmailcom>
2026-08-05 11:44:33 +0530

    day2

---
+++ day2/prg1.py
@@ -0,0 +1,10 @@
+list=[["name","Akash"],
+     ["age",23],
+      [["marks"],[23,44,55]] ]
+print(list[0][0],":",list[0][1])
+print(list[1][0],":",list[1][1])
+print(list[2][0],":",list[2][1])
+
+
+
+
